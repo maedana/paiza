f9f9a939fb1594b0604d21337d696c5291450882
maedana <maeda.na@gmail.com>
2017-07-14 23:35:02 +0900

    今度こそいいのでは

--- a/20.rb
+++ a/20.rb
@@ -1,33 +1,88 @@
 def f(l, n, m, line_text)
-  remaining_lines = lines(line_text)
-  current_position_h = 1
-  current_position_v = l.to_i
-  while (bottom = find_bottom(current_position_v, current_position_h, remaining_lines, n))
-    a, _b, c = bottom
-    current_position_h =
-      if current_position_h == a
-        a + 1
-      else
-        a
-      end
-    current_position_v = c
-    remaining_lines.delete(bottom)
+  # 縦棒ごとに高さ順でソートした配列を持つhashを用意
+  amida_tree = AmidaTree.new(l, n, m, line_text)
+  while amida_tree.next
   end
-  current_position_h
+  amida_tree.current_pos_h
 end
 
-def abc(line_text)
-  lines(line_text).transpose
-end
+class AmidaTree
+  attr_reader :sorted_tree, :current_pos_h
 
-def find_bottom(current_position_v, current_position_h, remaining_lines, n)
-  target_lines = remaining_lines.select { |a, b, _c| (a == current_position_h || a == current_position_h - 1) && b <= current_position_v }
-  target_lines.sort_by { |_a, b, _c| b }.last
-end
+  def initialize(l, n, m, line_text)
+    @l = l.to_i # 線の長さ
+    @n = n.to_i # 縦線数
+    @m = m.to_i # 横線数
+    @line_text = line_text
+    # 縦線ごとにソートされた横線の配列を持つHash
+    set_sorted_tree
+    @current_pos_h = 1
+    @current_pos_v = @l
+  end
 
-def lines(line_text)
-  line_text.split("\n").map do |line|
-    line.split(' ').map(&:to_i)
+  def [](n_index)
+    @sorted_tree[n_index]
+  end
+
+  def next
+    left = find_left
+    right = find_right
+    if left
+      if right
+        if left[2] > right[1]
+          @next = left
+          solve_left
+        else
+          @next = right
+          solve_right
+        end
+      else
+        @next = left
+        solve_left
+      end
+    else
+      if right
+        @next = right
+        solve_right
+      end
+    end
+  end
+
+  def find_left
+    if (lines = @sorted_tree[@current_pos_h - 1]) && !lines.size.zero?
+      lines.find { |d| d[2] < @current_pos_v }
+    end
+  end
+
+  def find_right
+    if (lines = @sorted_tree[@current_pos_h]) && !lines.size.zero?
+      lines.find { |d| d[1] < @current_pos_v }
+    end
+  end
+
+  def solve_left
+    @sorted_tree[@current_pos_h - 1].delete(@next)
+    @current_pos_v = @next[2]
+    @current_pos_h -= 1
+  end
+
+  def solve_right
+    @sorted_tree[@current_pos_h].delete(@next)
+    @current_pos_h += 1
+    @current_pos_v = @next[2]
+  end
+
+  private
+
+  def set_sorted_tree
+    @sorted_tree = Hash.new {|h, k| h[k] = [] }
+    @line_text.split("\n").each do |line|
+      definition = line.split(' ').map(&:to_i)
+      @sorted_tree[definition[0]] << definition
+    end
+    (1...@n).each do |n|
+      @sorted_tree[n].sort_by! { |_a, b, _c| -b }
+    end
   end
 end
 
--- a/20_spec.rb
+++ a/20_spec.rb
@@ -40,164 +40,60 @@ describe 'A-20' do
   end
 end
 
-describe 'abc' do
-  subject { abc(line_text) }
+describe AmidaTree do
+  describe 'sorted_tree' do
+    subject { amida_tree.sorted_tree }
 
-  let(:line_text) do
-    <<~EOS
-      1 3 1
-      3 2 2
-    EOS
-  end
-
-  it { is_expected.to eq [[1,3], [3,2], [1,2]]}
-end
-
-describe 'find_horizontal_bottom_line' do
-  subject { find_bottom(current_position_v, current_position_h, remaining_lines, 4) }
-
-  context 'パターン1' do
-    let(:current_position_h) { 1 }
-    let(:current_position_v) { 7 }
-
-    let(:remaining_lines) do
-      [
-        [1, 3, 1],
-        [3, 2, 2],
-        [2, 3, 5],
-        [3, 4, 4],
-        [1, 6, 6]
-      ]
+    let(:amida_tree) do
+      AmidaTree.new('7', '4', '5', line_text)
     end
 
-    it { is_expected.to eq [1, 6, 6] }
-  end
-
-  context 'パターン2' do
-    let(:current_position_h) { 2 }
-    let(:current_position_v) { 6 }
-
-    let(:remaining_lines) do
-      [
-        [1, 3, 1],
-        [3, 2, 2],
-        [2, 3, 5],
-        [3, 4, 4]
-      ]
+    let(:line_text) do
+      <<~EOS
+        1 3 1
+        3 2 2
+        2 3 5
+        3 4 4
+        1 6 6
+      EOS
     end
 
-    it { is_expected.to eq [2, 3, 5] }
-  end
-
-  context 'パターン3' do
-    let(:current_position_h) { 3 }
-    let(:current_position_v) { 5 }
-
-    let(:remaining_lines) do
-      [
-        [1, 3, 1],
-        [3, 2, 2],
-        [3, 4, 4]
-      ]
+    let(:expected_hash) do
+      {
+        1 => [
+          [1, 6, 6],
+          [1, 3, 1]
+        ],
+        2 => [
+          [2, 3, 5]
+        ],
+        3 => [
+          [3, 4, 4],
+          [3, 2, 2]
+        ]
+      }
     end
 
-    it { is_expected.to eq [3, 4, 4] }
+    it { is_expected.to eq expected_hash }
   end
 
-  context 'パターン4' do
-    let(:current_position_h) { 4 }
-    let(:current_position_v) { 4 }
+  describe '[]' do
+    subject { amida_tree[1] }
 
-    let(:remaining_lines) do
-      [
-        [1, 3, 1],
-        [3, 2, 2]
-      ]
+    let(:amida_tree) do
+      AmidaTree.new('7', '4', '5', line_text)
     end
 
-    it { is_expected.to eq [3, 2, 2] }
-  end
-
-  context 'パターン5' do
-    let(:current_position_h) { 4 }
-    let(:current_position_v) { 4 }
-
-    let(:remaining_lines) do
-      [
-        [1, 3, 1]
-      ]
+    let(:line_text) do
+      <<~EOS
+        1 3 1
+        3 2 2
+        2 3 5
+        3 4 4
+        1 6 6
+      EOS
     end
 
-    it { is_expected.to be_nil }
+    it { is_expected.to eq [[1, 6, 6], [1, 3, 1]] }
   end
-
-  context 'パターン6' do
-    let(:current_position_v) { 5 }
-    let(:current_position_h) { 1 }
-
-    let(:remaining_lines) do
-      [
-        [3, 3, 4],
-        [1, 3, 2],
-        [4, 2, 2],
-        [2, 1, 2],
-        [2, 4, 4],
-        [3, 1, 1],
-        [1, 4, 3],
-        [4, 3, 4]
-      ]
-    end
-
-    it { is_expected.to eq [1, 4, 3] }
-  end
-
-  context 'パターン7' do
-    let(:current_position_v) { 3 }
-    let(:current_position_h) { 2 }
-
-    let(:remaining_lines) do
-      [
-        [3, 3, 4],
-        [1, 3, 2],
-        [4, 2, 2],
-        [2, 1, 2],
-        [2, 4, 4],
-        [3, 1, 1],
-        [4, 3, 4]
-      ]
-    end
-
-    it { is_expected.to eq [1, 3, 2] }
-  end
-
-  context 'パターン8' do
-    let(:current_position_v) { 2 }
-    let(:current_position_h) { 1 }
-
-    let(:remaining_lines) do
-      [
-        [3, 3, 4],
-        [4, 2, 2],
-        [2, 1, 2],
-        [2, 4, 4],
-        [3, 1, 1],
-        [4, 3, 4]
-      ]
-    end
-
-    it { is_expected.to be_nil }
-  end
-end
-
-describe 'lines' do
-  subject { lines(line_text) }
-
-  let(:line_text) do
-    <<~EOS
-      1 3 1
-      3 2 2
-    EOS
-  end
-
-  it { is_expected.to eq [[1,3,1], [3,2,2]]}
 end
