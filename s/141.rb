827cea5c0bc3398fe479fada6088f2af389bd038
maedana <maeda.na@gmail.com>
2017-07-17 16:05:08 +0900

    うまくいかねーわ

--- s/141.rb
+++ s/141.rb
@@ -25,7 +25,7 @@ class Highlight
   def try_unhilight(s, e)
     ret = false
     @actives.each.with_index(0) do |(as, ae), i|
-      if as <= s && e <= ae
+      if (as..ae).cover?(s) && (as..ae).cover?(e)
         @actives[i] = [as, s - 1]
         @actives.insert(i + 1, [e, ae])
         ret = true
@@ -36,19 +36,36 @@ class Highlight
   end
 
   def try_partial_hilight(s, e)
-    ret = false
+    remove_candidates = []
+#    require 'byebug'
+#    byebug
+    # 完全カバーされてるのでいらなくなるもの
     @actives.each.with_index(0) do |(as, ae), i|
-      if s <= as && e <= ae
-        @actives[i] = [s, ae]
-        ret = true
-        break
-      elsif as <= s && ae <= e
-        @actives[i] = [as, e]
-        ret = true
-        break
+      if (s..e).cover?(as) && (s..e).cover?(ae)
+        remove_candidates << [as, ae]
+      else
+        # 左が重なる
+        if as <= s && ae <= e
+          @actives[i] = [as, e]
+        elsif as <= e
+          # 右が重なる
+          @actives[i] = [s, ae]
+        end
       end
     end
-    ret
+    remove_candidates.each do |c|
+      @actives.delete(c)
+    end
+
+    # 範囲の左側なので単に追加出来るもの
+    if before_global_start?(s)
+      just_highlit(s, e)
+    end
+
+    # 範囲の右側なので単に追加出来る
+    if after_global_end?(e)
+      just_highlit(s, e)
+    end
   end
 
   def just_highlit(s, e)
@@ -58,11 +75,7 @@ class Highlight
   def highlight(s, e)
     update_global_start(s)
     update_global_end(e)
-    if before_global_start?(e) || after_global_end?(s)
-      just_highlit(s, e)
-    else
-      try_unhilight(e, e) || try_partial_hilight(s, e) || just_highlit(s, e)
-    end
+    try_unhilight(s, e) || try_partial_hilight(s, e)
   end
 
   def update_global_start(s)
