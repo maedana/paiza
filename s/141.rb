e317b332bd8b73db27e7d142a829f1b3e282bc21
maedana <maeda.na@gmail.com>
2017-07-17 22:46:37 +0900

    サンプルは全部通るようになったけど全然駄目だった

--- s/141.rb
+++ s/141.rb
@@ -5,29 +5,35 @@ def f(input)
 end
 
 class Highlight
-  attr_reader :l, :n, :logs, :actives
+  attr_accessor :l, :n, :logs, :actives
 
   def initialize(input)
     lines = input.split("\n")
     @l, @n = lines[0].split(' ').map(&:to_i)
     @logs = lines[1..@n].map { |l| l.split(' ').map(&:to_i) }
     @actives = []
-    @gs = 0 # ハイライト全体の最初
-    @ge = 0 # ハイライト全体の最後
+    @gs = nil # ハイライト全体の最初
+    @ge = nil # ハイライト全体の最後
   end
 
   def run
     @logs.each do |s, e|
+      require 'pp'
+#      pp "入力: #{[s, e]}"
       highlight(s, e)
+#      pp "結果: #{@actives}"
+#      puts "===========================\n"
     end
   end
 
-  def try_unhilight(s, e)
+  def unhilight(s, e)
     ret = false
     @actives.each.with_index(0) do |(as, ae), i|
       if (as..ae).cover?(s) && (as..ae).cover?(e)
         @actives[i] = [as, s - 1]
-        @actives.insert(i + 1, [e, ae])
+        if e < ae
+          @actives.insert(i + 1, [e + 1, ae])
+        end
         ret = true
         break
       end
@@ -35,68 +41,77 @@ class Highlight
     ret
   end
 
-  def try_partial_hilight(s, e)
-    remove_candidates = []
-#    require 'byebug'
-#    byebug
-    # 完全カバーされてるのでいらなくなるもの
+  def highlight(s, e)
+    return if unhilight(s, e)
+    ns = s
+    ne = e
+
+    if @actives.empty?
+      @actives.push([ns, ne])
+      return
+    end
+
     @actives.each.with_index(0) do |(as, ae), i|
-      if (s..e).cover?(as) && (s..e).cover?(ae)
-        remove_candidates << [as, ae]
-      else
-        # 左が重なる
-        if as <= s && ae <= e
-          @actives[i] = [as, e]
-        elsif as <= e
-          # 右が重なる
-          @actives[i] = [s, ae]
+      # 範囲の左側なので単に追加出来る
+      if ne + 1 < as
+        @actives.unshift([s, e])
+        break
+      end
+
+      # 最初に繋がる
+      if as <= ne + 1 && s < as
+        @actives[i] =
+          if ae <= ne
+            while @actives[i + 1] && @actives[i + 1].last <= ne
+              @actives.delete_at(i + 1)
+            end
+            [ns, ne]
+          else
+            [ns, ae]
+          end
+        break
+      end
+
+      # 最後に繋がる
+      if ns <= ae + 1
+        @actives[i] = [as, ne]
+        while @actives[i + 1] && @actives[i + 1].first <= ne + 1
+          next_ae = @actives[i + 1].last
+          #@actives[i] = [as, @actives.delete_at(i + 1).last]
+          @actives[i] = [as, ne < next_ae ? next_ae : ne]
+          @actives.delete_at(i + 1)
+        end
+        break
+      end
+
+      # 間に挟まる/後ろに入る
+#      require 'byebug'
+#      byebug
+      if ae < ns
+        if (next_item = @actives[i + 1])
+          if ne < next_item.first
+            @actives.insert(i + 1, [ns, ne])
+            break
+          else
+#            @actives[i] = [ns, next_item.last]
+#            @actives.delete_at(i + 1)
+          end
+        else
+          @actives.push([s, e])
+          break
         end
       end
+
+#      if ae < ns
+#        @actives.push([s, e])
+##        break
+#      end
     end
-    remove_candidates.each do |c|
-      @actives.delete(c)
-    end
-
-    # 範囲の左側なので単に追加出来るもの
-    if before_global_start?(s)
-      just_highlit(s, e)
-    end
-
-    # 範囲の右側なので単に追加出来る
-    if after_global_end?(e)
-      just_highlit(s, e)
-    end
-  end
-
-  def just_highlit(s, e)
-    @actives << [s, e]
-  end
-
-  def highlight(s, e)
-    update_global_start(s)
-    update_global_end(e)
-    try_unhilight(s, e) || try_partial_hilight(s, e)
-  end
-
-  def update_global_start(s)
-    @gs = s if s < @gs
-  end
-
-  def update_global_end(e)
-    @ge = e if @ge < e
-  end
-
-  def before_global_start?(e)
-    e < @gs
-  end
-
-  def after_global_end?(s)
-    @ge < s
   end
 
   def active_count
     @actives.each.inject(0) do |sum, (s, e)|
-      sum + (e - s)
+      sum + (e - s + 1)
     end
   end
 end
--- s/141_spec.rb
+++ s/141_spec.rb
@@ -100,8 +100,12 @@ describe Highlight do
   end
 
   describe '#highlight' do
-    subject(:highlight) do
-      Highlight.new(input)
+    subject do
+      highlight = Highlight.new(input)
+      # xxxoooxxxoooxxxooo
+      highlight.actives = [[4, 6], [10, 12], [16, 18]]
+      highlight.highlight(s, e)
+      highlight.actives
     end
 
     let(:input) do
@@ -113,53 +117,174 @@ describe Highlight do
       EOS
     end
 
-    context '1回目' do
-      subject do
-        super().highlight(2, 6)
-        super().actives
-      end
+    context '最初に追加されるパターン' do
+      let(:s) { 1 }
+      let(:e) { 2 }
 
-      let(:expected) do
-        [
-          [2, 6]
-        ]
-      end
-
-      it { is_expected.to eq expected }
+      it { is_expected.to eq [[1, 2], [4, 6], [10, 12], [16, 18]] }
     end
 
-    context '2回目' do
-      subject do
-        super().highlight(2, 6)
-        super().highlight(6, 8)
-        super().actives
-      end
+    context '最初に繋がるパターン1' do
+      let(:s) { 1 }
+      let(:e) { 3 }
 
-      let(:expected) do
-        [
-          [2, 8]
-        ]
-      end
-
-      it { is_expected.to eq expected }
+      it { is_expected.to eq [[1, 6], [10, 12], [16, 18]] }
     end
 
-    context '3回目' do
-      subject do
-        super().highlight(2, 6)
-        super().highlight(6, 8)
-        super().highlight(3, 4)
-        super().actives
-      end
+    context '最初に繋がるパターン2' do
+      let(:s) { 1 }
+      let(:e) { 4 }
 
-      let(:expected) do
-        [
-          [2, 3],
-          [4, 8]
-        ]
-      end
+      it { is_expected.to eq [[1, 6], [10, 12], [16, 18]] }
+    end
 
-      it { is_expected.to eq expected }
+    context '最初に繋がるパターン3' do
+      let(:s) { 1 }
+      let(:e) { 6 }
+
+      it { is_expected.to eq [[1, 6], [10, 12], [16, 18]] }
+    end
+
+    context '最初に繋がるパターン4' do
+      let(:s) { 1 }
+      let(:e) { 7 }
+
+      it { is_expected.to eq [[1, 7], [10, 12], [16, 18]] }
+    end
+
+    context '最初に繋がるパターン5' do
+      let(:s) { 1 }
+      let(:e) { 9 }
+
+      it { is_expected.to eq [[1, 9], [10, 12], [16, 18]] }
+    end
+
+    context '最初に繋がるパターン6' do
+      let(:s) { 1 }
+      let(:e) { 12 }
+
+      it { is_expected.to eq [[1, 12], [16, 18]] }
+    end
+
+    context '最後に繋がるパターン1' do
+      let(:s) { 6 }
+      let(:e) { 7 }
+
+      # it { is_expected.to eq [[4, 6], [10, 12], [16, 18]] }
+      it { is_expected.to eq [[4, 7], [10, 12], [16, 18]] }
+    end
+
+    context '最後に繋がるパターン2' do
+      let(:s) { 7 }
+      let(:e) { 7 }
+
+      # it { is_expected.to eq [[4, 6], [10, 12], [16, 18]] }
+      it { is_expected.to eq [[4, 7], [10, 12], [16, 18]] }
+    end
+
+    context '最後に繋がるパターン3' do
+      let(:s) { 7 }
+      let(:e) { 8 }
+
+      # it { is_expected.to eq [[4, 6], [10, 12], [16, 18]] }
+      it { is_expected.to eq [[4, 8], [10, 12], [16, 18]] }
+    end
+
+    context '最後に繋がるパターン4' do
+      let(:s) { 7 }
+      let(:e) { 9 }
+
+      # it { is_expected.to eq [[4, 6], [10, 12], [16, 18]] }
+      it { is_expected.to eq [[4, 12], [16, 18]] }
+    end
+
+    context '最後に追加されるパターン1' do
+      let(:s) { 20 }
+      let(:e) { 22 }
+
+      it { is_expected.to eq [[4, 6], [10, 12], [16, 18], [20, 22]] }
+    end
+
+    context '最後に追加されるパターン2' do
+      let(:s) { 18 }
+      let(:e) { 22 }
+
+      it { is_expected.to eq [[4, 6], [10, 12], [16, 22]] }
+    end
+
+    context '最後に追加されるパターン3' do
+      let(:s) { 19 }
+      let(:e) { 22 }
+
+      it { is_expected.to eq [[4, 6], [10, 12], [16, 22]] }
+    end
+  end
+
+  describe '#highlightその2' do
+    subject do
+      highlight = Highlight.new(input)
+      highlight.actives = actives
+      highlight.highlight(s, e)
+      highlight.actives
+    end
+
+    let(:input) do
+      <<~EOS
+        10 3
+        2 6
+        6 8
+        3 4
+      EOS
+    end
+
+    context 'パターン1' do
+      let(:actives) { [[14, 14], [53, 59], [65, 77], [83, 100]] }
+      let(:s) { 43 }
+      let(:e) { 45 }
+
+      it { is_expected.to eq [[14, 14], [43, 45], [53, 59], [65, 77], [83, 100]] }
+    end
+
+    context 'パターン2' do
+      let(:actives) { [[6, 7], [14, 14], [43, 45], [53, 59], [65, 77], [83, 100]] }
+      let(:s) { 44 }
+      let(:e) { 45 }
+
+      it { is_expected.to eq [[6, 7], [14, 14], [43, 43], [53, 59], [65, 77], [83, 100]] }
+    end
+
+    context 'パターン3' do
+      let(:actives) { [[1, 1], [6, 6]] }
+      let(:s) { 1 }
+      let(:e) { 7 }
+
+      it { is_expected.to eq [[1, 7]] }
+    end
+  end
+
+  describe '#unhilight' do
+    subject do
+      highlight = Highlight.new(input)
+      highlight.actives = actives
+      highlight.unhilight(s, e)
+      highlight.actives
+    end
+
+    let(:input) do
+      <<~EOS
+        10 3
+        2 6
+        6 8
+        3 4
+      EOS
+    end
+
+    context 'パターン1' do
+      let(:actives) { [[43, 45]] }
+      let(:s) { 44 }
+      let(:e) { 45 }
+
+      it { is_expected.to eq [[43, 43]] }
     end
   end
 end
